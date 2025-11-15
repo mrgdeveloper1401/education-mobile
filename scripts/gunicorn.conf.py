1ef5b7f228600f367336027aa792e96c7f05d5e9
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2025-11-15 19:20:31 +0330

    ready production

--- scripts/gunicorn.conf.py
+++ scripts/gunicorn.conf.py
@@ -1,19 +1,19 @@
 # gunicorn.conf.py
 import multiprocessing
 
-bind = "0.0.0.0:8001"
+bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "uvicorn.workers.UvicornWorker"
 worker_connections = 1000
 
 # Logging
-loglevel = "info"
+loglevel = "error"
 errorlog = "-"
-accesslog = "-"
+accesslog = None
 
 # Timeouts
 timeout = 30
-keepalive = 2
+keepalive = 5
 graceful_timeout = 30
 
 # Process naming
