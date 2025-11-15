95aa74d8946e2d6fd4cc01b10a1d93ec9ff9bda7
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2025-11-15 21:18:13 +0330

    add command make migration and migrate into file start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 python manage.py collectstatic --noinput
+python manage.py makemigrations
+python manage.py migrate
 gunicorn base.asgi:application -c /home/app/scripts/gunicorn.conf.py
