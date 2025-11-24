bed23ffb0d26510adba19fe78958425be0519c1d
mohammad goodarzi <123094892+mrgdeveloper1401@users.noreply.github.com>
2025-11-24 13:04:26 +0330

    add create_user_score into script start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,5 @@
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
+python manage.py create_user_score
 gunicorn base.asgi:application -c /home/app/scripts/gunicorn.conf.py
